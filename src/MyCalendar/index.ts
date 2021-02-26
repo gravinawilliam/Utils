c54c3f2cb53a5602451ec519b00fdc1e64368f1f
William Gravina <60082243+gravinawilliam@users.noreply.github.com>
2021-02-26 11:15:30 -0300

    Update index.ts
    refactor: arrumei a posição da string 'Domingo' na lista weekdays

--- src/MyCalendar/index.ts
+++ src/MyCalendar/index.ts
@@ -15,13 +15,13 @@ export default class MyCalendar {
   ];
 
   readonly weekdays = [
+    'Domingo',
     'Segunda-feira',
     'Terça-feira',
     'Quarta-feira',
     'Quinta-feira',
     'Sexta-feira',
-    'Sábado',
-    'Domingo'
+    'Sábado'
   ];
 
   readonly weekdaysShort = [
